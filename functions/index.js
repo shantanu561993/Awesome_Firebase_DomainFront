9837220b43064ac7b0f99ee9e9af064a30f7cce3
Dan <51533129+the2dl@users.noreply.github.com>
2021-05-04 09:30:51 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,19 @@ var http = require('http'), httpProxy = require('http-proxy');
 
 var proxy = httpProxy.createProxyServer({secure:false,xfwd:true});
 
+/* Require a header, useful to require a particular header to access the page (helps hide origin C2) - Optional
+app.use('', (req, res, next) => {
+   if (req.headers.PutAUniqueHeaderHere) {
+       next();
+   } else {
+       res.send('{ "Error": "Invalid Authentication." }');
+	// You can  also do a generic 403,500,etc
+    	// res.status(403);
+   }
+});
+*/
+
+
 /* your C2 must have a URI . In this case I am using /api/" */
 app.all('/api/*', function(req, res, next){
     console.log(req.url);
